6cce27e53f96e8eed59876e6f1a11e93e6f3a42d
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-04-01 16:52:57 -0500

    added viewBinding on MainActivity.java

--- app/src/main/java/com/imaduddinsheikh/newsaggregator/MainActivity.java
+++ app/src/main/java/com/imaduddinsheikh/newsaggregator/MainActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.imaduddinsheikh.newsaggregator.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        com.imaduddinsheikh.newsaggregator.databinding
+                .ActivityMainBinding binding
+                = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setTitle("News Gateway");
     }
 }
